fb79c5352ff15b2313b27764f0a714f7d7b444e7
Kyle Schwent <kshwiss@gmail.com>
2023-12-26 20:33:53 -0600

    works locally

--- backend/server.js
+++ backend/server.js
@@ -21,9 +21,13 @@ app.use(
   })
 );
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "client/build")));
 app.use("/api", commentRouter);
-app.get("/", (req, res) => {
-  res.send("SERVER IS UP");
+// app.get("/", (req, res) => {
+//   res.send("SERVER IS UP");
+// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "../build/index.html"));
 });
 
 app.listen(PORT, () => {
